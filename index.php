e03de288a3695933afc76dcd9a0ec4562f232bcc
Ami777 <jakubkrol@o2.pl>
2015-07-21 14:23:46 +0200

    - Log for PHP built-in server

--- index.php
+++ index.php
@@ -18,14 +18,22 @@ $app->post(
     function () use ($app) {
         OnlyAllowedIPs();
 
-        $codeCompiler = new \hackerEarthV3CodeCompiler(HACKER_EARTH_API_SECRET);
+        error_log('Getting data...', 4);
+        error_log($app->request->getBody(), 4);
         $inputData = json_decode($app->request->getBody(), true);
         if (empty($inputData['lang'])) throw new Exception('Empty "lang" json param!');
         if (empty($inputData['code'])) throw new Exception('Empty "code" json param!');
+
+        error_log('Sending request...', 4);
+        $codeCompiler = new \hackerEarthV3CodeCompiler(HACKER_EARTH_API_SECRET);
         $ans = $codeCompiler->run($inputData['lang'], $inputData['code'], @$inputData['user_input']?:'');
+        error_log('Response:', 4);
+        error_log(json_encode($ans), 4);
+
+        error_log('Rendering response...', 4);
         $app->render(200, [
             'output_html'=>@$ans['run_status']['output_html']?:'',
-            'output'=>@$ans['run_status']['output']?:'',
+            'output'=>trim(@$ans['run_status']['output']?:''),
             'all_info'=>$ans
         ]);
     }
