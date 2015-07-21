1205c48b98f860093719ef6fd4192d5d355e955f
Ami777 <jakubkrol@o2.pl>
2015-07-21 13:07:39 +0200

    - config/secrets.inc.php remover from git...

--- config/secrets.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Define HackerEarth API key secret here
-define('HACKER_EARTH_API_SECRET', '');
-
-//Define your custom OnlyAllowedIPs function, it should throw an Exception when wrong IP.
-function OnlyAllowedIPs(){
-        return true;
-//        throw new Exception('Access denied.'); //Uncomment it to disallow IP
-}
